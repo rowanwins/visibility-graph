e5db3f9caecabce98d2d6687481cda89f1462c98
Rowan Winsemius <rowanwins@yahoo.com.au>
2022-02-14 13:59:41 +1100

    fix up docs biuld

--- debug/vite.config.js
+++ debug/vite.config.js
@@ -4,7 +4,7 @@ import gj from './gj'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/visibility-graph/',
+  base: '/visibility-graph/debug/dist/',
   plugins: [
     vue(), gj()
   ]
